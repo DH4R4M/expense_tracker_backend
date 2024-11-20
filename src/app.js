a546a267bb72ef1447eb457ad75cb1b6915c17a5
DH4R4M <gohildharamvirsinh1530@gmail.com>
2024-11-20 23:32:41 +0530

    add route

--- src/app.js
+++ src/app.js
@@ -1,22 +1,22 @@
-const express = require('express');
-const cors = require('cors');
-const expenseRoutes = require('./routes/expenseRoutes');
-require('dotenv').config();
+// const express = require('express');
+// const cors = require('cors');
+// const expenseRoutes = require('./routes/expenseRoutes');
+// require('dotenv').config();
 
-const app = express();
-
-
-app.use(cors());
-app.use(express.json());
+// const app = express();
 
 
-app.use('/api/expenses', expenseRoutes);
+// app.use(cors());
+// app.use(express.json());
 
 
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).json({ error: 'Something went wrong!' });
-});
+// app.use('/api/expenses', expenseRoutes);
 
 
-module.exports = app;
+// app.use((err, req, res, next) => {
+//   console.error(err.stack);
+//   res.status(500).json({ error: 'Something went wrong!' });
+// });
+
+
+// module.exports = app;
--- src/server.js
+++ src/server.js
@@ -1,4 +1,23 @@
-const app = require('./app');
+const express = require('express');
+const cors = require('cors');
+const expenseRoutes = require('./routes/expenseRoutes');
+require('dotenv').config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the Expense Tracker API!');
+});
+
+app.use('/api/expenses', expenseRoutes);
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ error: 'Something went wrong!' });
+});
 
 const PORT = process.env.PORT || 8080;
 
